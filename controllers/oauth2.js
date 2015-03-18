315b0a28b608ca115b7a43a4365433a7bf4338b4
Mohamad Atieh <atiyaleb@gmail.com>
2015-03-18 11:36:06 -0400

    Update oauth2.js

--- controllers/oauth2.js
+++ controllers/oauth2.js
@@ -25,38 +25,6 @@ server.deserializeClient(function(id, callback){
 });
 
 /**
-* Exchange the client id and password/secret for an access token.
-*
-* The callback accepts the `client`, which is exchanging the client's id and
-* password/secret from the token request for verification. If these values are validated, the
-* application issues an access token on behalf of the client who authorized the code.
-*/
-server.exchange(oauth2orize.exchange.clientCredentials(function (client, scope, done) {
-	var value = uid(config.token.accessTokenLength);
-	
-	//Pass in a null for user id since there is no user when using this grant type
-	var token = new Token();
-
-	token.value = value;
-	token.clientId = client._id; 
-	token.userId = null;
-	if(scope === 'undefined')
-		token.scope = null;
-	else
-		token.scope = scope;
-	token.expirationDate = config.token.calculateExpirationDate();
-
-
-	token.save(function (err) {
-	if (err) {
-		return done(err);
-	}
-		return done(null, token, {expires_in: config.token.expiresIn}); //use config
-	});
-
-}));
-
-/**
 * Exchange user id and password for access tokens.
 *
 * The callback accepts the `client`, which is exchanging the user's name and password
