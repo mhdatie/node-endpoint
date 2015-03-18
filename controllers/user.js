5b3bf6b8aa27813d1cff7177c1cf4621d4767774
Mohamad Atieh <atiyaleb@gmail.com>
2015-03-18 11:47:40 -0400

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -34,8 +34,10 @@ var createUser = function(req,res){
 						}
 					}
 					return res.json({message: 'New user created.', data: user});
-					//todo redirect to /oauth/token with username, password, and grant-type = offline_access
+					//redirect client to /oauth/token with data.user.username, data.user.password, and grant-type = offline_access
 					// to gain a refresh token.
+					
+					//-- all endpoints from this point on should use the BearerStrategy
 				});
 			}
 		});
